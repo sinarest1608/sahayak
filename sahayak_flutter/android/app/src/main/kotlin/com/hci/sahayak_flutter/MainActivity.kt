4e9b8daa77a659e728a30941862c6a27c7757781
Kshitij Sinha <43610780+sinarest1608@users.noreply.github.com>
2022-01-31 11:06:29 -0500

    Initial Commit

---
+++ sahayak_flutter/android/app/src/main/kotlin/com/hci/sahayak_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hci.sahayak_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sahayak_flutter/lib/app/modules/home/bindings/home_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'package:sahayak_flutter/app/modules/home/controllers/home_controller.dart';
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+  }
+}
