c48cacfcfcc47191c3c90073ad35e2035e21665b
Rajdev Kapoor <rajdev1409@gmail.com>
2022-04-08 11:09:54 -0400

    added app bar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -12,150 +12,157 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color(0xffF1F4FF),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Greetings,\nRajdev Kapoor",
-                      style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Color(0xffF1F4FF),
+          title: Text(
+            "Sahayak",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 18, bottom: 18, right: 18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Greetings,\nRajdev Kapoor",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.red,
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                child: Text(
+                  "🗓️ Happening Today",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                width: Get.width,
+                height: 165,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Color(0xff96bbff).withOpacity(0.7),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 6,
+                      blurRadius: 10,
+                      offset: Offset(0, 3), // changes position of shadow
                     ),
-                    CircleAvatar(
-                      backgroundColor: Colors.red,
-                    )
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 15.0),
-                  child: Text(
-                    "🗓️ Happening Today",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                child: Text(
+                  "👋 How can we help you today?",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-                Container(
+              ),
+              GestureDetector(
+                onTap: () {
+                  Get.to(() => AcademicsHomeWidget());
+                },
+                child: Container(
                   width: Get.width,
-                  height: 165,
+                  height: 157,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
-                    color: Color(0xff96bbff).withOpacity(0.7),
+                    color: Color(0xffe8eaff).withOpacity(0.7),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 6,
-                        blurRadius: 10,
+                        color: Colors.grey.withOpacity(0.4),
+                        spreadRadius: 3,
+                        blurRadius: 7,
                         offset: Offset(0, 3), // changes position of shadow
                       ),
                     ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 15.0),
-                  child: Text(
-                    "👋 How can we help you today?",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Get.to(() => AcademicsHomeWidget());
-                  },
-                  child: Container(
-                    width: Get.width,
-                    height: 157,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Color(0xffe8eaff).withOpacity(0.7),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.4),
-                          spreadRadius: 3,
-                          blurRadius: 7,
-                          offset: Offset(0, 3), // changes position of shadow
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "🏫",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "🏫",
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "Academics\nGuide",
+                            textAlign: TextAlign.right,
                             style: TextStyle(
                                 fontSize: 20, fontWeight: FontWeight.bold),
                           ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              "Academics\nGuide",
-                              textAlign: TextAlign.right,
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 35,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Get.to(() => HousingGuideView());
-                  },
-                  child: Container(
-                    width: Get.width,
-                    height: 157,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Color(0xfffee8eb),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.4),
-                          spreadRadius: 3,
-                          blurRadius: 7,
-                          offset: Offset(0, 3), // changes position of shadow
                         ),
                       ],
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "🏡",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Get.to(() => HousingGuideView());
+                },
+                child: Container(
+                  width: Get.width,
+                  height: 157,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color(0xfffee8eb),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.4),
+                        spreadRadius: 3,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "🏡",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "Housing\nGuide",
+                            textAlign: TextAlign.right,
                             style: TextStyle(
                                 fontSize: 20, fontWeight: FontWeight.bold),
                           ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              "Housing\nGuide",
-                              textAlign: TextAlign.right,
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: 15,
-                ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+            ],
           ),
         ));
   }
